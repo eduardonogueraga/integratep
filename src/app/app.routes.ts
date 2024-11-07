a5a3e74af3a2617c6fec39d1a78b279a73d1696e
Julián Fernández Calvo <julian.jfc@gmail.com>
2024-11-07 17:43:33 +0100

    cosas
    eegegegegeeg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
+
+
 //import { LayoutComponent } from '../components/layout/layout.component';
 //import DashboardComponent from "../components/courses/dashboard/dashboard.component"
 
 export const routes: Routes = [
-    { path: '',
-    loadComponent: () => import('../components/layout/layout.component')},
-    // Otras rutas...
-];
+    { path: 'dashboard',
+        loadComponent: () => import('../components/courses/dashboard/dashboard.component')
+    },
+    { 
+        path: '**', 
+        redirectTo: '', 
+        pathMatch: 'full' 
+    }
+]
+
+;
--- src/components/layout/layout.component.ts
+++ src/components/layout/layout.component.ts
@@ -3,13 +3,12 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from '../courses/dashboard/dashboard.component';
 import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SidebarComponent,DashboardComponent,AboutComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent,AboutComponent, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
